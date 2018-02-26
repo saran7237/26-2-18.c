a3c875d32657ac1d11e28f505c7705b6757a66f0
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-26 16:02:06 +0530

    Create 26.2.18.c

---
+++ 26.2.18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int n;
+clrscr ();
+printf("enter the n balue");
+scanf("%d",n);
+for(i=1;i<=1;i++)
+{
+prntf("%d%d%d%d;n,i+n);
+}
+return 0;
+}
